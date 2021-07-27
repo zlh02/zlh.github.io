91ae8335dc973ed0749678892709d6d2c500a75c
Agrry <2795153821@qq.com>
2021-07-27 22:34:36 +0800

    update

---
+++ js/Top.js
@@ -0,0 +1,9 @@
+const backTop2 = document.getElementById("backTop2")
+
+const TOP = document.getElementById("top")
+
+backTop2.addEventListener("click", function (e) {
+
+TOP.scrollIntoView({ behavior: "smooth" })
+
+})
